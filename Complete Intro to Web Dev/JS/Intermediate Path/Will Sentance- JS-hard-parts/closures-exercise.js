a20063b45034530d6b6187033d41a536b1513470
Praful Shrestha <prafulshrestha.softace@outlook.com>
2023-06-11 22:46:09 +0545

    exercise files

--- Complete Intro to Web Dev/JS/Intermediate Path/Will Sentance- JS-hard-parts/closures-exercise.js
+++ Complete Intro to Web Dev/JS/Intermediate Path/Will Sentance- JS-hard-parts/closures-exercise.js
@@ -116,9 +116,9 @@ function createFunction() {
   // /*** Uncomment these to check your work! ***/
   const called = function() { console.log('hello') };
   const afterCalled = after(3, called);
-  afterCalled(); // => nothing is printed
-  afterCalled(); // => nothing is printed
-  afterCalled(); // => 'hello' is printed
+//   afterCalled(); // => nothing is printed
+//   afterCalled(); // => nothing is printed
+//   afterCalled(); // => 'hello' is printed
   
   
   // CHALLENGE 6
